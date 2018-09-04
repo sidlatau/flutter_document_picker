9b4c290c3c61b35ba99d30c82830ffe9c79d9018
Tautvydas Šidlauskas <tautvydas.sidlauskas@gmail.com>
2018-09-04 20:42:33 +0300

    iOS part: picked file is moved to temp dir. Path (not absolute string) is returned.

--- ios/Classes/SwiftFlutterDocumentPickerDelegate.swift
+++ ios/Classes/SwiftFlutterDocumentPickerDelegate.swift
@@ -38,7 +38,24 @@ public class SwiftFlutterDocumentPickerDelegate: NSObject {
 
 extension SwiftFlutterDocumentPickerDelegate: UIDocumentPickerDelegate {
     public func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentAt url: URL) {
-        flutterResult?(url.absoluteString)
+        // Create file URL to temporary folder
+        var tempUrl = URL(fileURLWithPath: NSTemporaryDirectory())
+        // Apend filename (name+extension) to URL
+        tempUrl.appendPathComponent(url.lastPathComponent)
+        do {
+            // If file with same name exists remove it (replace file with new one)
+            if FileManager.default.fileExists(atPath: tempUrl.path) {
+                try FileManager.default.removeItem(atPath: tempUrl.path)
+            }
+            // Move file from app_id-Inbox to tmp/filename
+            try FileManager.default.moveItem(atPath: url.path, toPath: tempUrl.path)
+            flutterResult?(tempUrl.path)
+        } catch {
+            print(error.localizedDescription)
+            flutterResult?(nil)
+        }
+
+
     }
 
     public func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
