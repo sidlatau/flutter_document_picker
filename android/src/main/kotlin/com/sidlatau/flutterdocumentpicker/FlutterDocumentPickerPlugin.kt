634184d6c7521ffdbd93d4f0d8882657f3e9dca4
Robert Odrowaz <robert@arcuilo.com>
2020-01-14 01:18:13 +0100

    Extract setup method

--- android/src/main/kotlin/com/sidlatau/flutterdocumentpicker/FlutterDocumentPickerPlugin.kt
+++ android/src/main/kotlin/com/sidlatau/flutterdocumentpicker/FlutterDocumentPickerPlugin.kt
@@ -1,14 +1,16 @@
 package com.sidlatau.flutterdocumentpicker
 
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
-class FlutterDocumentPickerPlugin(
-        private val delegate: FlutterDocumentPickerDelegate
-) : MethodCallHandler {
+class FlutterDocumentPickerPlugin : MethodCallHandler {
+    private var delegate: FlutterDocumentPickerDelegate? = null
+
     companion object {
         const val TAG = "flutter_document_picker"
         private const val ARG_ALLOWED_FILE_EXTENSIONS = "allowedFileExtensions"
@@ -20,26 +22,39 @@ class FlutterDocumentPickerPlugin(
             if (registrar.activity() == null) {
                 // When a background flutter view tries to register the plugin, the registrar has no activity.
                 // We stop the registration process as this plugin is foreground only.
-                return;
+                return
             }
 
-            val channel = MethodChannel(registrar.messenger(), "flutter_document_picker")
+            val plugin = FlutterDocumentPickerPlugin()
+            plugin.setup(registrar.messenger(), registrar, null)
+        }
+    }
 
-            val delegate = FlutterDocumentPickerDelegate(
+    private fun setup(messenger: BinaryMessenger, registrar: Registrar?,
+                      activityBinding: ActivityPluginBinding?) {
+        var delegate: FlutterDocumentPickerDelegate? = null
+
+        if (registrar != null) {
+            delegate = FlutterDocumentPickerDelegate(
                     activity = registrar.activity()
             )
-
             registrar.addActivityResultListener(delegate)
-
-            channel.setMethodCallHandler(
-                    FlutterDocumentPickerPlugin(delegate)
+        } else if (activityBinding != null) {
+            delegate = FlutterDocumentPickerDelegate(
+                    activity = activityBinding.activity
             )
+            activityBinding.addActivityResultListener(delegate)
         }
+
+        this.delegate = delegate
+
+        val channel = MethodChannel(messenger, "flutter_document_picker")
+        channel.setMethodCallHandler(this)
     }
 
     override fun onMethodCall(call: MethodCall, result: Result) {
         if (call.method == "pickDocument") {
-            delegate.pickDocument(
+            delegate?.pickDocument(
                     result,
                     allowedFileExtensions = parseArray(call, ARG_ALLOWED_FILE_EXTENSIONS),
                     allowedMimeTypes = parseArray(call, ARG_ALLOWED_MIME_TYPES),
